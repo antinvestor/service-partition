55173f8483e3a7c3c49a143f3af055fa4b675ce2
Peter Bwire <bwire517@gmail.com>
2021-06-07 10:04:31 +0300

    Update the base tenant and partition appropriately

--- migrations/0001/20210514_create_default_tenant.sql
+++ migrations/0001/20210514_create_default_tenant.sql
@@ -1,3 +1,6 @@
+-- to quickly generate xids use : https://play.golang.org/p/xSMJJ9G9Lt_-
 
-INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'antinvestor', 'Default base tenant that all others build on');
-INSERT INTO partitions (id, tenant_id, partition_id, name, description) VALUES('c2f4j7au6s7f91uqnokg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'antinvestor', 'Default base partition that all other partitions build on');
+-- Default base partition
+INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'System Manager', 'Default base tenant that all others build on');
+INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties) VALUES('c2f4j7au6s7f91uqnokg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'System manager', 'Default base partition in the base tenant', '{"scope": "openid offline offline_access profile contact", "audience": ["service.partition", "service.profile", "service.notification", "service.files", "service.ledger"], "logo_uri": "https://static.antinvestor.com/logo.png", "redirect_uris": ["https://system.antinvestor.com/callback.html"]}');
+
