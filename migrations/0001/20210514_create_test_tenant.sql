b8297827c7d4c5703a2aad8edb24e16fcc60395a
Peter Bwire <bwire517@gmail.com>
2021-06-18 08:31:32 +0300

    fix issue with migrations

--- migrations/0001/20210514_create_test_tenant.sql
+++ migrations/0001/20210514_create_test_tenant.sql
@@ -2,5 +2,5 @@
 -- Test base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg', 'Testing Manager', 'Default test tenant that all others build on');
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
-    VALUES('9bsv0s3pbdv002o80qhg', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg', 'Testing partition', 'Default base partition for testing in base tenant', '{"scope": "openid offline offline_access profile contact", "audience": ["service.partition", "service.profile", "service.notification", "service.files", "service.ledger"], "logo_uri": "https://static.antinvestor.com/logo.png", "redirect_uris": ["http://localhost:8070/callback.html","http://127.0.0.1:8070/callback.html"]}');
+    VALUES('9bsv0s3pbdv002o80qhg', '9bsv0s3pbdv002o80qfg', '9bsv0s3pbdv002o80qhg', 'Testing partition', 'Default base partition for testing in base tenant', '{"scope": "openid offline offline_access profile contact", "audience": ["service_partition", "service_profile", "service_notification", "service_files", "service_ledger", "service_lostmyid"], "logo_uri": "https://static.antinvestor.com/logo.png", "redirect_uris": ["http://localhost:8070/callback.html","http://127.0.0.1:8070/callback.html"]}');
 
