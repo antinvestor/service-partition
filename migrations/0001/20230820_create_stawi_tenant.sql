5c1974927fb9e9dcf3d85842e7fd6362e3707230
Peter Bwire <bwire517@gmail.com>
2023-08-20 15:46:55 +0300

    add in stawi tenants to the system

---
+++ migrations/0001/20230820_create_stawi_tenant.sql
@@ -0,0 +1,10 @@
+-- to quickly generate xids use : https://play.golang.org/p/xSMJJ9G9Lt_-
+
+-- Default base partition
+INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bsv0s0hijjg02qks6jg', '9bsv0s0hijjg02qks6jg', '9bsv0s0hijjg02qks6kg', 'Stawi Development', 'Default base tenant for testing and building stawi');
+INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
+    VALUES('9bsv0s0hijjg02qks6kg', '9bsv0s0hijjg02qks6jg', '9bsv0s0hijjg02qks6kg',
+           'Stawi', 'Default stawi partition to serve the masses',
+           '{"scope": "openid offline offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], ' ||
+           '"logo_uri": "https://static.stawi.io/logo.png", "redirect_uris": ["https://app.stawi.io/callback.html"]}');
+
