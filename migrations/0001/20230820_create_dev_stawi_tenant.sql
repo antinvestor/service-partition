1835582d8a59807544311a785489c378b4e26df8
Peter Bwire <bwire517@gmail.com>
2023-08-20 17:46:08 +0300

    bugfix json to add properties to partition table

--- migrations/0001/20230820_create_dev_stawi_tenant.sql
+++ migrations/0001/20230820_create_dev_stawi_tenant.sql
@@ -5,6 +5,5 @@ INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bs
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
     VALUES('9bsv0s0hijjg02qks6i0', '9bsv0s0hijjg02qks6dg', '9bsv0s0hijjg02qks6i0',
            'Stawi Development', 'Default stawi development partition in stawi development',
-           '{"scope": "openid offline offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], ' ||
-           '"logo_uri": "https://static.stawi.io/logo.png", "redirect_uris": ["https://app-dev.stawi.io/callback.html"]}');
+           '{"scope": "openid offline offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], "logo_uri": "https://static.stawi.io/logo.png", "redirect_uris": ["https://app-dev.stawi.io/callback.html"]}');
 
