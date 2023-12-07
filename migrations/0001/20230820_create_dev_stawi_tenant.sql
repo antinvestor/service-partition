b346477bba8bdb5ae60fcee48dcf03d0890006de
Peter Bwire <bwire517@gmail.com>
2023-12-07 07:14:53 +0300

    update stawi test oauth2 records

--- migrations/0001/20230820_create_dev_stawi_tenant.sql
+++ migrations/0001/20230820_create_dev_stawi_tenant.sql
@@ -5,5 +5,5 @@ INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bs
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
     VALUES('9bsv0s0hijjg02qks6i0', '9bsv0s0hijjg02qks6dg', '9bsv0s0hijjg02qks6i0',
            'Stawi Development', 'Default stawi development partition in stawi development',
-           '{"scope": "openid offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], "logo_uri": "https://static.stawi.io/logo.png", "redirect_uris": ["https://app-dev.stawi.io", "http://localhost:40455"]}');
+           '{"scope": "openid offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], "logo_uri": "https://static.stawi.io/logo.png", "redirect_uris": ["https://app-dev.stawi.io/index.html", "http://localhost:40455/index.html", "http://127.0.0.1::40455/index.html"]}');
 
