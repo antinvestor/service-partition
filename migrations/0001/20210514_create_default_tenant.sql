9a9d689b746cdf17127f72e4f83b9e6963343dea
Peter Bwire <bwire517@gmail.com>
2024-05-29 09:12:23 +0300

    modify the system to allow tenants us admin interface

--- migrations/0001/20210514_create_default_tenant.sql
+++ migrations/0001/20210514_create_default_tenant.sql
@@ -3,5 +3,5 @@
 -- Default base partition
 INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'System Manager', 'Default base tenant that all others build on');
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, properties)
-    VALUES('c2f4j7au6s7f91uqnokg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'System manager', 'Default base partition in the base tenant', '{"scope": "openid offline offline_access profile contact", "audience": ["service_partition", "service_profile", "service_notification", "service_files", "service_ledger"], "logo_uri": "https://static.antinvestor.com/logo.png", "redirect_uris": ["https://system.antinvestor.com/callback.html"]}');
+    VALUES('c2f4j7au6s7f91uqnokg', 'c2f4j7au6s7f91uqnojg', 'c2f4j7au6s7f91uqnokg', 'System manager', 'Default base partition in the base tenant', '{"scope": "openid offline offline_access profile contact", "audience": ["service_partition", "service_profile", "service_notification", "service_payments", "service_files", "service_ledger"], "logo_uri": "https://static.antinvestor.com/logo.png", "redirect_uris": ["https://admin.stawi.io/callback/auth"]}');
 
