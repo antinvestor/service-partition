d1500ec1834c065f400cdad7927db34b79252917
Peter Bwire <bwire517@gmail.com>
2024-12-23 10:53:32 +0300

    minor edits

--- migrations/0001/20230820_create_stawi_tenant.sql
+++ migrations/0001/20230820_create_stawi_tenant.sql
@@ -5,5 +5,5 @@ INSERT INTO tenants (id, tenant_id, partition_id, name, description) VALUES('9bs
 INSERT INTO partitions (id, tenant_id, partition_id, name, description, client_secret, properties)
     VALUES('9bsv0s0hijjg02qks6kg', '9bsv0s0hijjg02qks6jg', '9bsv0s0hijjg02qks6kg',
            'Stawi', 'Default stawi partition to serve the masses', 'Sec_oXrpEHfunPu6r4A58f',
-           '{"scope": "openid offline_access profile contact", "audience": ["service_chat_engine", "service_profile", "service_stawi_api", "service_files"], "logo_uri": "https://static.chamamobile.com/logo.png", "redirect_uris": ["https://matrix.chamamobile.com/_matrix/v3/login/sso/callback"]}');
+           '{"scope": "openid offline_access profile contact", "audience": ["service_matrix", "service_profile", "service_stawi_api", "service_files"], "logo_uri": "https://static.chamamobile.com/logo.png", "redirect_uris": ["https://matrix.chamamobile.com/_matrix/v3/login/sso/callback"]}');
 
