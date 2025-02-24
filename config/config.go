1a77b62c366eaab39f71b38fb18bfa316ffed333
Peter Bwire <bwire517@gmail.com>
2025-02-24 13:42:05 +0300

    upgrade docker file errors

--- config/config.go
+++ config/config.go
@@ -3,7 +3,7 @@ package config
 import "github.com/pitabwire/frame"
 
 type PartitionConfig struct {
-	*frame.ConfigurationDefault
+	frame.ConfigurationDefault
 
 	NotificationServiceURI       string `envDefault:"127.0.0.1:7020" env:"NOTIFICATION_SERVICE_URI"`
 	QueuePartitionSyncURL        string `envDefault:"mem://partition_sync_hydra" env:"QUEUE_PARTITION_SYNC"`
