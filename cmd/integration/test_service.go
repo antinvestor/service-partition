fc8839399780fba19a08cec128e8da4c8d9b403b
Peter Bwire <bwire517@gmail.com>
2025-02-27 14:37:23 +0300

    upgrade partition service appropriately to utilize latest frame

--- cmd/integration/test_service.go
+++ cmd/integration/test_service.go
@@ -5,11 +5,7 @@ import (
 	"fmt"
 	apis "github.com/antinvestor/apis/go/common"
 	partitionv1 "github.com/antinvestor/apis/go/partition/v1"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 	"log"
-	"net/url"
-	"strings"
 )
 
 // Unary interceptor to log metadata and request body
@@ -17,26 +13,6 @@ import (
 func main() {
 
 	ctx := context.Background()
-
-	endpointValues := url.Values{}
-	audienceList := strings.Join([]string{"service_profile", "service_partition", "service_files", "service_notification", "service_payment", "service_stawi"}, " ")
-	endpointValues.Add("audience", audienceList)
-
-	tokenClient := &clientcredentials.Config{
-		ClientID:       "dc8e8598-89d5-4983-818e-50fbb6933498",
-		ClientSecret:   "b1QQBzBM04CDvEX9heDx",
-		TokenURL:       "https://oauth2.chamamobile.com/oauth2/token",
-		Scopes:         []string{},
-		EndpointParams: endpointValues,
-		AuthStyle:      oauth2.AuthStyleAutoDetect,
-	}
-
-	token, err := tokenClient.Token(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create token for client: %v", err)
-	}
-
-	log.Printf("Got token : %s", token.AccessToken)
 
 	partitionCli, err := partitionv1.NewPartitionsClient(ctx,
 		apis.WithEndpoint("localhost:50051"),
