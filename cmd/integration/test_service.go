238a7c7595ee3bd56670fe8ef8dfbcf3f1995e7e
Peter Bwire <bwire517@gmail.com>
2025-02-27 13:31:33 +0300

    cleanup partition service

--- cmd/integration/test_service.go
+++ cmd/integration/test_service.go
@@ -5,24 +5,52 @@ import (
 	"fmt"
 	apis "github.com/antinvestor/apis/go/common"
 	partitionv1 "github.com/antinvestor/apis/go/partition/v1"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 	"log"
+	"net/url"
+	"strings"
 )
+
+// Unary interceptor to log metadata and request body
 
 func main() {
 
 	ctx := context.Background()
+
+	endpointValues := url.Values{}
+	audienceList := strings.Join([]string{"service_profile", "service_partition", "service_files", "service_notification", "service_payment", "service_stawi"}, " ")
+	endpointValues.Add("audience", audienceList)
+
+	tokenClient := &clientcredentials.Config{
+		ClientID:       "dc8e8598-89d5-4983-818e-50fbb6933498",
+		ClientSecret:   "b1QQBzBM04CDvEX9heDx",
+		TokenURL:       "https://oauth2.chamamobile.com/oauth2/token",
+		Scopes:         []string{},
+		EndpointParams: endpointValues,
+		AuthStyle:      oauth2.AuthStyleAutoDetect,
+	}
+
+	token, err := tokenClient.Token(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create token for client: %v", err)
+	}
+
+	log.Printf("Got token : %s", token.AccessToken)
+
 	partitionCli, err := partitionv1.NewPartitionsClient(ctx,
 		apis.WithEndpoint("localhost:50051"),
 		apis.WithTokenEndpoint("https://oauth2.chamamobile.com/oauth2/token"),
 		apis.WithTokenUsername("dc8e8598-89d5-4983-818e-50fbb6933498"),
 		apis.WithTokenPassword("b1QQBzBM04CDvEX9heDx"),
-		apis.WithAudiences("service_profile", "service_partition", "service_files", "service_notification", "service_payment", "service_stawi"))
+		apis.WithAudiences("service_profile", "service_partition", "service_files", "service_notification", "service_payment", "service_stawi"),
+	)
+
 	if err != nil {
 		log.Fatalf("Failed to create partition client: %v", err)
 	}
 
-	log.Println("Partition client token : ", partitionCli.GetInfo())
-
+	partitionCli.SetInfo()
 	partition, err := partitionCli.GetPartition(ctx, "9bsv0s0hijjg02qks6kg")
 	if err != nil {
 		log.Fatalf("Failed to get partition: %v", err)
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.3.0
 	github.com/pitabwire/frame v1.30.0
 	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/oauth2 v0.25.0
 	google.golang.org/grpc v1.70.0
 	gorm.io/datatypes v1.2.5
 )
@@ -61,7 +62,6 @@ require (
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/oauth2 v0.25.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
